0fc28017a405d7278c85bf10a72eda5abd90432a
Alex <alexandralaudon@gmail.com>
2015-02-19 13:47:04 -0700

    Fixed Destroy Bug (and added private event_params method)

---
+++ spec/features/events_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'Events' do
+
+  scenario 'User edits an event' do
+    Event.create!(
+    description: 'My aweeesome event'
+    )
+
+    visit root_path
+    expect(page).to have_content('My aweeesome event')
+    click_on 'edit'
+    fill_in 'Description', with: 'My awesome event'
+    click_on 'Update Event'
+
+    expect(page).to have_content('My awesome event')
+    expect(page).to have_no_content('My aweeesome event')
+  end
+
+  scenario 'User deletes an event' do
+    Event.create!(
+    description: 'Hello World'
+    )
+
+    visit root_path
+    expect(page).to have_content('Hello World')
+    click_on 'delete'
+    expect(page).to have_no_content('Hello World')
+  end
+
+
+  
+end
